ffe3a42cb65698d297a8a545968b3038b02675ab
Ronny Standtke <ronny.standtke@fhnw.ch>
2013-04-26 13:58:22 +0200

    added missing "\"

--- wheezy-pu/functions.sh
+++ wheezy-pu/functions.sh
@@ -21,7 +21,7 @@ build_image()
 {
 
 	# update time stamp in bootloaders
- 	sed -i "s|<version its:translate=\"no\">.*</version>|<version its:translate=\"no\">(Version ${TODAY})</version>|1"
+ 	sed -i "s|<version its:translate=\"no\">.*</version>|<version its:translate=\"no\">(Version ${TODAY})</version>|1" \
  		config/bootloaders/isolinux/xmlboot.config
 	sed -i "s|title-text.*|title-text: \"Lernstick-Prüfungsumgebung Debian 7 (Version ${TODAY})\"|1" \
 		config/includes.binary/boot/grub/themes/lernstick/theme.txt 
