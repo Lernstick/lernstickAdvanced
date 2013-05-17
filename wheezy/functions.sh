9d9a0ecb2960d35c37e9532c4150f0aea49a067f
Ronny Standtke <ronny.standtke@fhnw.ch>
2013-05-17 18:35:23 +0200

    added build scripts for wheezy

---
+++ wheezy/functions.sh
@@ -0,0 +1,90 @@
+init_build()
+{
+	START=$(date)
+	TODAY=$(date +%Y-%m-%d)
+}
+
+configure_cd()
+{
+	ISO_SUFFIX="_bootcd"
+	SYSTEM_SUFFIX=" Boot-CD"
+#	mv config/chroot_local-packageslists/lernstick_squeeze.list config/chroot_local-packageslists/lernstick_squeeze
+#	mv config/chroot_local-packageslists/bootcd config/chroot_local-packageslists/bootcd.list
+}
+
+configure()
+{
+	echo ""
+}
+
+build_image()
+{
+
+	# update time stamp in bootloaders
+ 	sed -i "s|<version its:translate=\"no\">.*</version>|<version its:translate=\"no\">(Version ${TODAY})</version>|1" \
+ 		config/bootloaders/isolinux/xmlboot.config
+	sed -i "s|title-text.*|title-text: \"Lernstick Debian 7 (Version ${TODAY})\"|1" \
+		config/includes.binary/boot/grub/themes/lernstick/theme.txt 
+
+	# update configuration
+	rm -f config/binary
+	rm -f config/bootstrap
+	rm -f config/chroot
+	rm -f config/common
+	rm -f config/source
+	lb clean
+	lb config \
+		--apt-recommends false \
+		--architectures i386 \
+		--archive-areas "main contrib non-free" \
+		--binary-images iso \
+		--distribution wheezy \
+		--firmware-chroot true \
+		--iso-volume "lernstick${SYSTEM_SUFFIX} ${TODAY}" \
+		--linux-packages linux-image-3.8-1 \
+		--mirror-binary http://ftp.ch.debian.org/debian/ \
+		--mirror-bootstrap http://ftp.ch.debian.org/debian/ \
+		--mirror-chroot http://ftp.ch.debian.org/debian/ \
+		--mirror-chroot-security http://security.debian.org/ \
+		--mode debian \
+		--source ${SOURCE} \
+		--verbose
+
+	# build image (and produce a log file)
+	lb build 2>&1 | tee logfile.txt
+
+	ISO_FILE="binary.iso"
+	if [ -f ${ISO_FILE} ]
+	then
+		PREFIX="lernstick_debian7${ISO_SUFFIX}_${TODAY}"
+		IMAGE="${PREFIX}.iso"
+		mv ${ISO_FILE} ${IMAGE}
+		mv ${ISO_FILE}.zsync ${IMAGE}.zsync
+		echo "Creating MD5 for iso..."
+		md5sum ${IMAGE} > ${IMAGE}.md5
+
+		if [ "${SOURCE}" = "true" ]
+		then
+			# debian live sources
+			mv source.debian-live.tar ${PREFIX}_source.debian-live.tar
+
+			# debian sources
+			DEBIAN_TAR="${PREFIX}_source.debian.tar"
+			mv source.debian.tar ${DEBIAN_TAR}
+			md5sum ${DEBIAN_TAR} > ${DEBIAN_TAR}.md5
+		fi
+
+		# move files from tmpfs to harddisk
+		mv ${PREFIX}* /home/ronny/lernstick/build/
+	else
+		echo "Error: ISO file was not build"
+	fi
+
+	echo "Start: ${START}" | tee -a logfile.txt
+	echo "Stop : $(date)" | tee -a logfile.txt
+	mv logfile.txt /home/ronny/lernstick/build/
+
+	# hello, wake up!!! :-)
+	#eject
+}
+
