850f11439205c1fd23b5ada5e49ae00c80ca89d1
Ronny Standtke <ronny.standtke@fhnw.ch>
2013-10-18 13:51:45 +0200

    no more owncloud in exam environment (was a bad idea), fixed package lists

--- wheezy-pu/build_dvd_online.sh
+++ wheezy-pu/build_dvd_online.sh
@@ -8,7 +8,6 @@ cp config/bootloaders/isolinux/xmlboot.config .
 cp config/includes.binary/boot/grub/grub.cfg .
 cp variants/online/grub.cfg config/includes.binary/boot/grub/
 cp variants/online/online.list.chroot config/package-lists/
-cp variants/online/root_password.chroot config/hooks/
 cp variants/online/xmlboot.config config/bootloaders/isolinux/
 mv config/package-lists/offline.list.chroot .
 
@@ -22,5 +21,4 @@ build_image
 cp grub.cfg config/includes.binary/boot/grub/
 cp xmlboot.config config/bootloaders/isolinux/
 mv offline.list.chroot config/package-lists/
-rm config/hooks/root_password.chroot
 rm config/package-lists/online.list.chroot
