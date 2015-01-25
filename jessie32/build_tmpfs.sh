789d1c13ab9889ab0e54c85af012469225103f1b
Ronny Standtke <ronny.standtke@gmx.net>
2015-01-25 16:11:41 +0100

    separate config trees for jessie 32 bit and 64 bit

---
+++ jessie32/build_tmpfs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TMPFS="/mytmpfs/lernstick"
+
+rm -rf "${TMPFS}"
+mkdir "${TMPFS}"
+
+PWD="$(pwd)"
+
+cp -a "${PWD}/config" "${TMPFS}"
+ln -s "${PWD}/build_dvd.sh" "${TMPFS}"
+ln -s "${PWD}/build_source.sh" "${TMPFS}"
+ln -s "${PWD}/cache" "${TMPFS}"
+ln -s "${PWD}/constants" "${TMPFS}"
+ln -s "${PWD}/functions.sh" "${TMPFS}"
