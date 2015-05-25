b56fc635515e6644c0c74890f4d70a6f21b638d9
Ronny Standtke <ronny.standtke@gmx.net>
2015-05-25 11:46:48 +0200

    re-enabled grub timestamp

--- jessie64/functions.sh
+++ jessie64/functions.sh
@@ -23,12 +23,14 @@ build_image()
 {
 
 	# update time stamp in bootloaders
+	# ISOLINUX/SYSLINUX
  	sed -i "s|<version its:translate=\"no\">.*</version>|<version its:translate=\"no\">(Version ${TODAY})</version>|1" \
  		config/bootloaders/isolinux/bootlogo.dir/xmlboot.config
 	gfxboot --archive config/bootloaders/isolinux/bootlogo.dir --pack-archive config/bootloaders/isolinux/bootlogo
 	cp config/bootloaders/isolinux/bootlogo config/bootloaders/isolinux/bootlogo.orig
-#	sed -i "s|title-text.*|title-text: \"Lernstick Debian 8 (Version ${TODAY})\"|1" \
-#		config/includes.binary/boot/grub/themes/lernstick/theme.txt 
+	# GRUB
+	sed -i "s|title-text.*|title-text: \"Lernstick Debian 8 (Version ${TODAY})\"|1" \
+		config/includes.binary/boot/grub/themes/lernstick/theme.txt 
 
 	# update configuration
 	rm -f config/binary
