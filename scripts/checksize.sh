6fede0638bb8f2bf291237675003702c6b24534a
Ronny Standtke <ronny.standtke@gmx.net>
2016-07-03 21:18:22 +0200

    performance improvement in checksize script

--- scripts/checksize.sh
+++ scripts/checksize.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script creates a CSV file that includes packages and the
 # size sum of the package itself and its autoremovable dependencies.
@@ -7,6 +7,7 @@
 
 echo "package,autoremove size,removed packages" > checksize.csv
 
+declare -A SIZE_CACHE
 
 for i in $(dpkg -l | grep ^ii | awk '{ print $2 }')
 do
@@ -15,11 +16,19 @@ do
 	AUTOREMOVE_PACKAGES="$(apt-get -s autoremove $i | grep ^Remv | awk '{ print $2 }')"
 	for j in ${AUTOREMOVE_PACKAGES}
 	do
-		SIZE=$(apt-cache show --no-all-versions $j | grep ^Size | awk '{ print $2 }')
-		echo "   size of $j: ${SIZE}"
+		if [ ${SIZE_CACHE[$j]+_} ]
+		then
+			SIZE=${SIZE_CACHE[$j]}
+			echo "   size of $j: ${SIZE} (cached)"
+		else
+			SIZE=$(apt-cache show --no-all-versions $j | grep ^Size | awk '{ print $2 }')
+			SIZE_CACHE[$j]=${SIZE}
+			echo "   size of $j: ${SIZE}"
+		fi
 		AUTOREMOVE_SIZE=$((${AUTOREMOVE_SIZE} + ${SIZE}))
 	done
 	echo "   ===> autoremove size: ${AUTOREMOVE_SIZE}"
+	echo "   ---> cache size: ${#SIZE_CACHE[@]}"
 	echo -n "$i,${AUTOREMOVE_SIZE}," >> checksize.csv
         for j in ${AUTOREMOVE_PACKAGES}
 	do
