0ff279692c652b2c72923ff3b272f1fe8562975e
Ronny Standtke <ronny.standtke@fhnw.ch>
2017-03-31 15:10:20 +0200

    improved grub hardware detection (added workaround for another Mac)

--- scripts/hwdetect.c
+++ scripts/hwdetect.c
@@ -18,7 +18,9 @@
  *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <grub/charset.h>
 #include <grub/dl.h>
+#include <grub/efi/efi.h>
 #include <grub/env.h>
 #include <grub/err.h>
 #include <grub/extcmd.h>
@@ -30,8 +32,14 @@
 
 GRUB_MOD_LICENSE ("GPLv3+");
 
-static int intel_hd_graphics_3000 = 0;
+// known vendors
+static int apple = 0;
+
+// known graphic cards
 static int amd_radeon_hd_6600M_series = 0;
+static int intel_hd_graphics_3000 = 0;
+static int intel_hd_graphics_arrandale = 0;
+static int nvidia_geforce_gt_330m= 0;
 
 static int
 grub_hwdetect_iter (grub_pci_device_t dev __attribute__ ((unused)), grub_pci_id_t pciid,
@@ -41,6 +49,11 @@ grub_hwdetect_iter (grub_pci_device_t dev __attribute__ ((unused)), grub_pci_id_
   // e.g. "8086:0116" -> 0x01168086 = 18251910
 
   switch(pciid) {
+    case 4620422:
+      // PCI ID "8086:0046"
+      intel_hd_graphics_arrandale=1;
+      break;
+
     case 18251910:
       // PCI ID "8086:0116"
       intel_hd_graphics_3000=1;
@@ -49,6 +62,11 @@ grub_hwdetect_iter (grub_pci_device_t dev __attribute__ ((unused)), grub_pci_id_
     case 1732317186:
       // PCI ID "1002:6741"
       amd_radeon_hd_6600M_series=1;
+      break;
+
+    case 170463454:
+      // PCI ID "10DE:0A29"
+      nvidia_geforce_gt_330m=1;
       break;
   }
 
@@ -60,9 +78,29 @@ grub_cmd_hwdetect (grub_extcmd_context_t ctxt __attribute__ ((unused)),
 		int argc __attribute__ ((unused)),
 		char **args __attribute__ ((unused)))
 {
+
+  // check EFI vendor
+  const grub_efi_system_table_t *st = grub_efi_system_table;
+  char *vendor;
+  grub_uint16_t *vendor_utf16;
+  for (vendor_utf16 = st->firmware_vendor; *vendor_utf16; vendor_utf16++);
+  vendor = grub_malloc (4 * (vendor_utf16 - st->firmware_vendor) + 1);
+  if (vendor) {
+    *grub_utf16_to_utf8 ((grub_uint8_t *) vendor, st->firmware_vendor, vendor_utf16 - st->firmware_vendor) = 0;
+    if (grub_strcmp (vendor, "Apple") == 0) {
+      apple=1;
+    }
+    grub_free (vendor);
+  }
+
+  // check for known graphic cards
   grub_pci_iterate (grub_hwdetect_iter, NULL);
-  if (intel_hd_graphics_3000 && amd_radeon_hd_6600M_series) {
-    grub_env_set ("detected_hw", "Mac_A1286");
+
+  // detect known (problematic) hardware
+  if (apple && intel_hd_graphics_3000 && amd_radeon_hd_6600M_series) {
+    grub_env_set ("detected_hw", "Mac_A1286_Radeon");
+  } else if (apple && intel_hd_graphics_arrandale && nvidia_geforce_gt_330m) {
+    grub_env_set ("detected_hw", "Mac_A1286_NVIDIA");
   } else {
     grub_env_set ("detected_hw", "unknown");
   }
