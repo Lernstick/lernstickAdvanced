385401eafa952b2904da10df66866c27a870eeee
Ronny Standtke <ronny.standtke@gmx.net>
2017-07-10 20:00:37 +0200

    updated source build script

--- build_source.sh
+++ build_source.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+
+ISO_SUFFIX="_standard"
 SOURCE="true"
 
 . ./functions.sh
@@ -17,6 +20,21 @@ else
 		exit 0
 	fi
 fi
+
+if [ ! -d "${BUILD_DIR}" ]
+then
+	echo "ERROR: build directory ${BUILD_DIR} doesn't exist."
+	echo "Please check the BUILD_DIR definition in your constants file."
+	exit 0
+fi
+
+# copy additional package lists to config directory
+cp templates/debian_main-standard.list.chroot config/package-lists/
+cp templates/itch.list.chroot config/package-lists/
+cp templates/lernstick-standard.list.chroot config/package-lists/
+
+# remove uninstall hook of mini version
+rm -f config/hooks/live/uninstall_packages-mini.chroot
 
 init_build
 configure
