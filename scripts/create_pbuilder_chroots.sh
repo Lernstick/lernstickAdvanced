1838051395d3ce2fc32a93fcbbbddff51634ff9d
Ronny Standtke <ronny.standtke@gmx.net>
2019-09-07 12:00:11 +0200

    updated pbuilder chroots script for Debian 10

--- scripts/create_pbuilder_chroots.sh
+++ scripts/create_pbuilder_chroots.sh
@@ -2,8 +2,7 @@
 
 BIND_MOUNT=/home/debian10/lernstick/backports/buster/
 LS_KEYRING=/tmp/lernstick-10.gpg
-#OTHER_MIRRORS="deb http://security.debian.org/ buster/updates main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-thirdparty-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-staging main contrib non-free"
-OTHER_MIRRORS="deb http://security.debian.org/ buster/updates main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports-staging main contrib non-free"
+OTHER_MIRRORS="deb http://security.debian.org/ buster/updates main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-thirdparty-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-staging main contrib non-free"
 
 # pbuilder needs the "dearmored" version of our keyfile
 gpg --yes --output $LS_KEYRING --dearmor $(dirname $0)/../config/archives/lernstick-10.key
