6ce6a2f26c13f1eb0eb5569664262382a6eb4db0
Ronny Standtke <ronny.standtke@gmx.net>
2020-12-27 17:27:16 +0100

    upgraded to Linux kernel 5.9

--- functions.sh
+++ functions.sh
@@ -99,7 +99,7 @@ build_image()
                 --distribution buster \
 		--firmware-chroot false \
                 --iso-volume "lernstick${SYSTEM_SUFFIX} ${TODAY}" \
-		--linux-packages linux-image-5.8.0-0.bpo.2 \
+		--linux-packages linux-image-5.9.0-0.bpo.2 \
                 --mirror-binary ${MIRROR_SYSTEM} \
                 --mirror-binary-security ${MIRROR_SECURITY_SYSTEM} \
                 --mirror-bootstrap ${MIRROR_BUILD} \
