61f8208910a813cac8b153b8f671620ac8c57bdc
Ronny Standtke <ronny.standtke@gmx.net>
2021-04-04 23:30:27 +0200

    first customizations for Debian 11 (still very experimental)

--- scripts/create_pbuilder_chroots.sh
+++ scripts/create_pbuilder_chroots.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-BIND_MOUNT=/home/debian10/lernstick/backports/buster/
-LS_KEYRING=/tmp/lernstick-10.gpg
-OTHER_MIRRORS="deb http://security.debian.org/ buster/updates main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-backports-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-thirdparty-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-10-staging main contrib non-free"
+BIND_MOUNT=/home/debian11/lernstick/backports/bullseye/
+LS_KEYRING=/tmp/lernstick-11.gpg
+#OTHER_MIRRORS="deb http://security.debian.org/ bullseye/updates main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-11-backports main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-11-backports-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-11-thirdparty-staging main contrib non-free|deb http://packages.lernstick.ch/lernstick lernstick-11-staging main contrib non-free"
+OTHER_MIRRORS=""
 
 # pbuilder needs the "dearmored" version of our keyfile
-gpg --yes --output $LS_KEYRING --dearmor $(dirname $0)/../config/archives/lernstick-10.key
+gpg --yes --output $LS_KEYRING --dearmor $(dirname $0)/../config/archives/lernstick-11.key
 
-# main version (Buster 64 Bit)
+# main version (Bullseye 64 Bit)
 pbuilder \
 	--create \
-	--basetgz /var/cache/pbuilder/base-buster-bpo.tar.gz \
+	--basetgz /var/cache/pbuilder/base-bullseye-bpo.tar.gz \
 	--bindmounts $BIND_MOUNT \
 	--debootstrapopts --include=apt-transport-https,ca-certificates,openssl \
-	--distribution buster \
+	--distribution bullseye \
 	--keyring $LS_KEYRING \
 	--othermirror "$OTHER_MIRRORS"
 
@@ -21,9 +22,9 @@ pbuilder \
 pbuilder \
 	--create \
 	--architecture i386 \
-	--basetgz /var/cache/pbuilder/base-buster32-bpo.tar.gz \
+	--basetgz /var/cache/pbuilder/base-bullseye32-bpo.tar.gz \
 	--bindmounts $BIND_MOUNT \
 	--debootstrapopts --include=apt-transport-https,ca-certificates,openssl \
-	--distribution buster \
+	--distribution bullseye \
 	--keyring $LS_KEYRING \
 	--othermirror "$OTHER_MIRRORS"
