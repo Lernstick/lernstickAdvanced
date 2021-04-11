6e00474903482552c579b994cfc941c96ce77f0b
Ronny Standtke <ronny.standtke@gmx.net>
2021-04-12 01:38:14 +0200

    got rid of mini version

--- build_iso.sh
+++ build_iso.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ISO_SUFFIX="_standard"
+ISO_SUFFIX=""
 SOURCE="false"
 
 . ./functions.sh
@@ -27,15 +27,6 @@ then
 	echo "Please check the BUILD_DIR definition in your constants file."
 	exit 0
 fi
-
-# copy additional package lists to config directory
-cp templates/debian_main-standard.list.chroot config/package-lists/
-cp templates/itch.list.chroot config/package-lists/
-cp templates/lernstick-standard.list.chroot config/package-lists/
-cp templates/signal.list.chroot config/package-lists/
-
-# remove uninstall hook of mini version
-rm -f config/hooks/live/uninstall_packages-mini.chroot
 
 init_build
 configure
