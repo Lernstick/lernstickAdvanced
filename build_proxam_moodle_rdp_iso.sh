cd6c8f104f0b5271036c36acd4cda25e5247e5c6
FilipWjk <wojtasikfilip21@gmail.com>
2026-04-14 19:47:34 +0200

    fix(ci): remove interactive tmpfs prompt from moodle-rdp build script

--- build_proxam_moodle_rdp_iso.sh
+++ build_proxam_moodle_rdp_iso.sh
@@ -11,21 +11,16 @@ check_and_source_constants
 if [ -d "${TMPFS_IMAGE_MOUNT}" ]
 then
 	cd "${TMPFS_IMAGE_MOUNT}"
-else
-	echo "The tmpfs directory \"${TMPFS_IMAGE_MOUNT}\" doesn't exist."
-	echo "Do you want to run the build without a tmpfs? (y/n)"
-	read answer
-	if [ "${answer}" != "y" ]
-	then
-		exit 0
-	fi
+elif [ -n "${TMPFS_IMAGE_MOUNT}" ]
+then
+	echo "WARNING: tmpfs directory \"${TMPFS_IMAGE_MOUNT}\" doesn't exist, building without tmpfs."
 fi
 
 if [ -n "${BUILD_DIR}" ] && [ ! -d "${BUILD_DIR}" ]
 then
 	echo "ERROR: build directory ${BUILD_DIR} doesn't exist."
 	echo "Please check the BUILD_DIR definition in your constants file."
-	exit 0
+	exit 1
 fi
 
 prepare_moodle_rdp_profile()
